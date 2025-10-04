2689d1f2f769cc4d9be90cafd71ecdc435da55a8
Danyx3k <Danyx3k@gmail.com>
2025-10-03 22:59:03 -0500

    Inicial

--- src/main/java/co/edu/uco/nose/Buisness/Domain/CityDomain.java
+++ src/main/java/co/edu/uco/nose/Buisness/Domain/CityDomain.java
@@ -6,29 +6,29 @@ import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
 import java.util.UUID;
 
 public final class CityDomain extends Domain {
-        private String name;
+    private String name;
 
-        public CityDomain() {
-            super(UUIDHelper.getUUIDHelper().getDefault());
-            setName(TextHelper.getDefault());
-        }
-
-        public CityDomain(final UUID id) {
-            super(id);
-            setName(TextHelper.getDefault());
-        }
-
-        public CityDomain(final UUID id, final String name){
-            super(id);
-            setName(name);
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(final String name) {
-            this.name = TextHelper.getDefaultwithTrim(name);
-        }
+    public CityDomain() {
+        super(UUIDHelper.getUUIDHelper().getDefault());
+        setName(TextHelper.getDefault());
     }
 
+    public CityDomain(final UUID id) {
+        super(id);
+        setName(TextHelper.getDefault());
+    }
+
+    public CityDomain(final UUID id, final String name){
+        super(id);
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = TextHelper.getDefaultwithTrim(name);
+    }
+
+}
--- src/main/java/co/edu/uco/nose/Buisness/Domain/IdTypeDomain.java
+++ src/main/java/co/edu/uco/nose/Buisness/Domain/IdTypeDomain.java
@@ -1,4 +1,34 @@
 package co.edu.uco.nose.Buisness.Domain;
 
-public class IdTypeDomain {
+import co.edu.uco.nose.crosscuting.helper.TextHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+
+import java.util.UUID;
+
+public final class IdTypeDomain extends Domain {
+    private String name;
+
+    public IdTypeDomain() {
+        super(UUIDHelper.getUUIDHelper().getDefault());
+        setName(TextHelper.getDefault());
+    }
+
+    public IdTypeDomain(final UUID id) {
+        super(id);
+        setName(TextHelper.getDefault());
+    }
+
+    public IdTypeDomain(final UUID id, final String name){
+        super(id);
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = TextHelper.getDefaultwithTrim(name);
+    }
 }
+
