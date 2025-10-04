b36c82f940277268959004ed7c5787977f323059
Danyx3k <Danyx3k@gmail.com>
2025-10-03 22:30:44 -0500

    Inicial

--- src/main/java/co/edu/uco/nose/Buisness/Domain/CityDomain.java
+++ src/main/java/co/edu/uco/nose/Buisness/Domain/CityDomain.java
@@ -1,4 +1,36 @@
 package co.edu.uco.nose.Buisness.Domain;
 
+import co.edu.uco.nose.crosscuting.helper.TextHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+
+import java.util.UUID;
+
 public class CityDomain {
+    public final class CityDomain extends Domain {
+        private String name;
+
+        public CityyDomain() {
+            super(UUIDHelper.getUUIDHelper().getDefault());
+            setName(TextHelper.getDefault());
+        }
+
+        public CityDomain(final UUID id) {
+            super(id);
+            setName(TextHelper.getDefault());
+        }
+
+        public CityDomain(final UUID id, final String name){
+            super(id);
+            setName(name);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = TextHelper.getDefaultwithTrim(name);
+        }
+    }
+
 }
