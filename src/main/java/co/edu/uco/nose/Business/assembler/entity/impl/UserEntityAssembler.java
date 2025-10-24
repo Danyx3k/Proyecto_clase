feab0d8edc0dd30d4409d5bc5641f95615f1d022
Danyx3k <Danyx3k@gmail.com>
2025-10-24 12:04:15 -0500

    Actualizaciones última clase ultimo COMMIT

--- src/main/java/co/edu/uco/nose/Business/assembler/entity/impl/UserEntityAssembler.java
+++ src/main/java/co/edu/uco/nose/Business/assembler/entity/impl/UserEntityAssembler.java
@@ -2,9 +2,15 @@ package co.edu.uco.nose.Business.assembler.entity.impl;
 
 import co.edu.uco.nose.Business.Domain.UserDomain;
 import co.edu.uco.nose.Business.assembler.entity.EntityAssambler;
+import co.edu.uco.nose.crosscuting.helper.ObjectHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
 import co.edu.uco.nose.entity.UserEntity;
 
 import java.util.List;
+import java.util.UUID;
+
+import static co.edu.uco.nose.Business.assembler.entity.impl.CityEntityAssembler.getCityEntityAssembler;
+import static co.edu.uco.nose.Business.assembler.entity.impl.IdTypeEntityAssembler.getIdTypeEntityAssembler;
 
 
 public final class UserEntityAssembler implements EntityAssambler <UserEntity, UserDomain>{
@@ -22,12 +28,52 @@ public final class UserEntityAssembler implements EntityAssambler <UserEntity, U
 
     @Override
     public UserEntity toEntity(final UserDomain domain) {
-        return null;
+        var domainTmp = ObjectHelper.getDefault(domain, new UserDomain(UUIDHelper.getUUIDHelper().getDefault()));
+        var idTytpeEntityTmp = getIdTypeEntityAssembler().toEntity(domainTmp.getIdType());
+        var cityEntityTmp = getCityEntityAssembler().toEntity(domainTmp.getCity());
+        
+        return new UserEntity(
+                domainTmp.getId(),
+                idTytpeEntityTmp,
+                domainTmp.getIdNumber(),
+                domainTmp.getFirstName(),
+                domainTmp.getSecondName(),
+                domainTmp.getFirstSurname(),
+                domainTmp.getSecondSurname(),
+                cityEntityTmp,
+                domainTmp.geteMail(),
+                domainTmp.getMobileNumber(),
+                domainTmp.iseMailConfirmed(),
+                domainTmp.iseMailConfirmedDefaultValue(),
+                domainTmp.isMobileNumberConfirmed(),
+                domainTmp.isMobileNumberConfirmedDefaultValue()
+        );
+        
     }
 
     @Override
     public UserDomain toDomain(final UserEntity entity) {
-        return null;
+        var entityTmp = ObjectHelper.getDefault(entity,new UserEntity());
+
+        var idTypeDominTmp = getIdTypeEntityAssembler().toDomain(entityTmp.getIdType());
+        var cityDomainTmp = getCityEntityAssembler().toDomain(entityTmp.getCity());
+
+        return new UserDomain(
+                entityTmp.getId(),
+                idTypeDominTmp,
+                entityTmp.getIdNumber(),
+                entityTmp.getFirstName(),
+                entityTmp.getSecondName(),
+                entityTmp.getFirstSurname(),
+                entityTmp.getSecondSurname(),
+                cityDomainTmp,
+                entityTmp.geteMail(),
+                entityTmp.getMobileNumber(),
+                entityTmp.iseMailConfirmed(),
+                entityTmp.iseMailConfirmedDefaultValue(),
+                entityTmp.isMobileNumberConfirmed(),
+                entityTmp.isMobileNumberConfirmedDefaultValue()
+                );
     }
 
     @Override
